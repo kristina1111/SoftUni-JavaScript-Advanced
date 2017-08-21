484f8f227797ab018798e99225ab65991605b34d
kristina1111 <kristina.k.kovacheva@gmail.com>
2017-08-21 09:56:51 +0300

    minor refactoring

--- 08.ObjectComposition-Lab/E03.ListProcessor.js
+++ 08.ObjectComposition-Lab/E03.ListProcessor.js
@@ -1,27 +1,27 @@
 let processor = (function (){
     let output = [];
+    let manager = {
+        add : (thing) => {
+            output.push(thing);
+        },
+        remove : (thing) =>{
+            // functional removing of matching element
+            output = output.filter(e => e!==thing);
 
+            // OR procedural core
+            // let found = output.indexOf(thing);
+            // while (output.indexOf(thing) !== -1) {
+            //     output.splice(found, 1);
+            //     found = output.indexOf(thing);
+            // }
+        },
+        print : ()=>{
+            // return output.join(',');
+            console.log(output.join(','))
+        }
+    };
     return function (arr) {
-        let manager = {
-            add : (thing) => {
-                output.push(thing);
-            },
-            remove : (thing) =>{
-                // functional removing of matching element
-                output = output.filter(e => e!==thing);
 
-                // OR procedural core
-                // let found = output.indexOf(thing);
-                // while (output.indexOf(thing) !== -1) {
-                //     output.splice(found, 1);
-                //     found = output.indexOf(thing);
-                // }
-            },
-            print : ()=>{
-                // return output.join(',');
-                console.log(output.join(','))
-            }
-        };
         arr.forEach(e=>{
             let tokens = e.split(' ');
             manager[tokens[0]](tokens[1]);
